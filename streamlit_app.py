67f78b5b1622816c55b051d5b340c4dc1277db2b
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-03-24 16:38:28 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if "selected_model" not in st.session_state:
 # Define model details
 models = {
     "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
-    "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"}
+    "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
     "mixtral-8x7b-32768": {"name": "Mixtral-8x7b-Instruct-v0.1", "tokens": 32768, "developer": "Mistral"},
 }
 
